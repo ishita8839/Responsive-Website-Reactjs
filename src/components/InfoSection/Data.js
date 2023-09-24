b8b55d85d0e0b6f23e4bde730d8080e03f1dfe8f
Ishita Malik <malikishita805@gmail.com>
2023-09-24 15:44:35 +0530

    first

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+    id:'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transaction without getting charged any fees',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/vehicle.png'),
+    alt:'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+
+export const homeObjTwo = {
+    id:'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 'We have covered no matter where you are located . All you need is an internet connection and a phone or phone number',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/money.png'),
+    alt:'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id:'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is extremely easy',
+    description: 'Get everything set up and ready in under 10 minutes. Al you need to do is add your information and you are ready yo go',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/files.png'),
+    alt:'Paper',
+    dark: false,
+    primary: false,
+    darkText: true
+};
